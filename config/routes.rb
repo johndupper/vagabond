3b60ee651ac82cd07b462ae5c7d2ed8f5764602c
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2017-01-26 15:58:54 -0500

    added root splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# root route "splash page"
+	get "splash/index"
+	root "splash#index"
 end
