b475e9e62093b5079ec3c3773e49850c33cf2916
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2017-01-27 12:54:34 -0500

    commented out unnecessary city CRUD

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,38 +3,38 @@ class CitiesController < ApplicationController
 		@cities = City.all
 	end
 	
-	def new 
-		@city = City.new
-	end
+#	def new 
+#		@city = City.new
+#	end
 	
-	def create
-		@city = City.create!(city_params)
-		if @city.save
-			redirect_to cities_url
-		else
-			redirect_to new_city_url
-		end
-	end
+#	def create
+#		@city = City.create!(city_params)
+#		if @city.save
+#			redirect_to cities_url
+#		else
+#			redirect_to new_city_url
+#		end
+#	end
 	
 	def show
 		@city = City.find(params[:id])
 	end
 	
-	def edit
-		@city = City.find(params[:id])
-	end
+#	def edit
+#		@city = City.find(params[:id])
+#	end
 	
-	def update
-		@city = City.find(params[:id])
-		@city.update(city_params)
-		redirect_to cities_path
-	end
-	
-	def destroy 
-		@city = City.find(params[:id])
-		@city.destroy
-		redirect_to cities_path
-	end
+#	def update
+#		@city = City.find(params[:id])
+#		@city.update(city_params)
+#		redirect_to cities_path
+#	end
+#	
+#	def destroy 
+#		@city = City.find(params[:id])
+#		@city.destroy
+#		redirect_to cities_path
+#	end
 	
 	private 
 	def city_params
