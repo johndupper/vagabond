1f3db73848a00ee19b9d39b2aa47eb92475a7c27
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2017-01-27 16:47:21 -0500

    removed extra line

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170127181655) do
 
   # These are extensions that must be enabled in order to support this database
