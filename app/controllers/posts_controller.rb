29a66a6f536a15cc5333c2ee74bb4c5185c901d6
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2017-01-29 11:28:03 -0500

    added styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,19 +23,16 @@ class PostsController < ApplicationController
 #      redirect_to post_path(@post)
 #    end
 
- # def edit
-  #   @post = Post.find(params[:id])
-  # end
-  #
-  # def update
-  #   @post = Post.find(params[:id])
-  #
-  #   if @post.update(post_params)
-  #     redirect_to @post
-  #   else
-  #     render 'edit'
-  #   end
-  # end
+  def edit
+     @post = Post.find(params[:id])
+   end
+	
+	def update
+    @city = City.find(params[:city_id])
+	@post = @city.posts.update(post_params)
+	
+	redirect_to city_path(@city)
+  	end  
 
  def destroy
 	@city = City.find(params[:city_id])
